1b692c21736c0aefb8e544ac1f86844d71b7b237
Raghavendra Prasad K <rprasadk@gmail.com>
2013-06-21 11:43:01 +1000

    Pushing new changes

--- source/app/web/core/CommandRegistry.cs
+++ source/app/web/core/CommandRegistry.cs
@@ -1,10 +1,29 @@
-﻿namespace app.web.core
+﻿using System;
+using System.Collections.Generic;
+
+namespace app.web.core
 {
   public class CommandRegistry : IFindCommands
   {
+      private readonly IEnumerable<IProcessOneRequest> listOfCommands;
+
+      public CommandRegistry(IEnumerable<IProcessOneRequest> listOfCommands)
+      {
+          this.listOfCommands = listOfCommands;
+      }
+
     public IProcessOneRequest get_command_that_can_process(IContainRequestInformation request)
     {
-      throw new System.NotImplementedException();
+        foreach (var command in listOfCommands)
+        {
+            if (command.can_process(request))
+            {
+                return command;
+            }
+        }
+
+        // TBD - return a null object
+        throw new Exception();
     }
   }
 }
